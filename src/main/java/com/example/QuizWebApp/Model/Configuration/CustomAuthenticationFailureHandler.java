57041d35c8f6bb1293f473341501b6b63d6f9e06
NobleTK <noble91knight@gmail.com>
2023-04-28 01:18:47 +0200

    Creating CustomAuthenticationFailureHandler.java, adding failureHandler to SecurityConfiguration.java,modifying @Id generation type in Question.java, adding authentication failure error message.

---
+++ src/main/java/com/example/QuizWebApp/Model/Configuration/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,20 @@
+package com.example.QuizWebApp.Model.Configuration;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        request.getSession().setAttribute("error", "Invalid username or password");
+        response.sendRedirect("/login");
+    }
+}
+
--- src/main/java/com/example/QuizWebApp/Model/Configuration/SecurityConfiguration.java
+++ src/main/java/com/example/QuizWebApp/Model/Configuration/SecurityConfiguration.java
@@ -35,6 +35,7 @@ public class SecurityConfiguration {
                         .anyRequest().authenticated())
                 .formLogin(formLogin -> formLogin
                         .loginPage("/login")
+                        .failureHandler(new CustomAuthenticationFailureHandler())
                         .defaultSuccessUrl("/")
                         .permitAll())
                 .logout(logout -> logout
